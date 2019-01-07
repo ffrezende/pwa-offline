8588e232ad4e289a2f3d2831268cdf6e9d19d919
Fabricio <fabricio.fraga.rezende@ibm.com>
2019-01-07 18:16:38 -0200

    localstorage

---
+++ js/mural/Mural.js
@@ -0,0 +1,38 @@
+const Mural = (function (_render, Filtro) {
+    "use strict"
+    let cartoes = JSON.parse(localStorage.getItem("cartoes")) || []
+        .map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
+    const render = () => _render({ cartoes: cartoes, filtro: Filtro.tagsETexto });
+
+    Filtro.on("filtrado", render)
+
+    render();
+
+    const salvaCartoes = function() {
+        localStorage.setItem("cartoes", JSON.stringify(
+            cartoes.map(cartao => ({ conteudo: cartao.conteudo, tipo: cartao.tipo })
+            ))
+        )};
+
+    function adiciona(cartao) {
+        if (logado) {
+            cartoes.push(cartao)
+            salvaCartoes()
+            cartao.on("mudanca.**", render)
+            cartao.on("remocao", () => {
+                cartoes = cartoes.slice(0)
+                cartoes.splice(cartoes.indexOf(cartao), 1)
+                render()
+            })
+            render()
+            return true
+        } else {
+            alert("Você não está logado")
+        }
+    }
+
+    return Object.seal({
+        adiciona
+    })
+
+})(Mural_render, Filtro)
